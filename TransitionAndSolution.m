6d3d928a91747a7a0d199c31dd7a33c4ec6ced86
Caoyang Shao <2963684236@qq.com>
2024-06-21 20:50:33 +0800

    no2
    完成零输入时系统状态变量和系统输出的求解和绘图

--- TransitionAndSolution.m
+++ TransitionAndSolution.m
@@ -9,10 +9,12 @@ c
 disp('状态转移矩阵为:');
 W
 %仿真数据初始化
+WhetherInputZero = input('是否指定输入u?(若不指定，则默认为0)[y/n]','s');
 WhetherX0Zero = input('你需要给定x0然后解出状态方程吗?(若不指定，则默认为0)[y/n]','s');
 switch WhetherX0Zero
     case 'n'
         x0 = [0;0;0;0;0];
+        xx0 = [0;0;0;0;0];
     case 'y'
         systemOrder = size(A, 1);
         fprintf('系统的阶数为 %d，因此您需要输入初始状态的维数为 %d 。', systemOrder, systemOrder);
@@ -21,21 +23,6 @@ switch WhetherX0Zero
         x0 = input('输入初始状态：');
         xx0 = inv(W)*x0;
 end
-t=0:0.01:3; 
-n=length(t);
-x=zeros(5,n); 
-xx=zeros(5,n);
-%求解状态变量和输出
-for i=1:n
-xx(:,i)=expm(L*t(i))*xx0;
-x(:,i)=W*xx(:,i);
-end
-plot(t,x)
-title('System Time Response with u=0');
-xlabel('Time-sec'); ylabel('Response-value');
-
-
-WhetherInputZero = input('是否指定输入u?(若不指定，则默认为0)[y/n]','s');
 switch WhetherInputZero
     case 'n'
         u = [0;0];
@@ -54,25 +41,23 @@ switch WhetherInputZero
                 u = dirac(tau);
         end
 end
-
-
-
-
-
-% 计算齐次解
-x_h = eAt * x0;
-% 计算特解
-integrand = expm(A * (t - tau)) * B * u;
-if tau == 0
-    x_p = 0;
-else
-    x_p = int(integrand, tau, 0, t);
+t=0:0.01:3; 
+n=length(t);
+x=zeros(5,n); 
+xx=zeros(5,n);
+%求解状态变量和输出
+for i=1:n
+    xx(:,i)=expm(L*t(i))*xx0;
+    x(:,i)=W*xx(:,i);
+    y = C*x;
 end
-xt = x_h + x_p;
-fprintf('状态方程解的表达式为\n')
-disp(xt)
-yt = C*xt;
-fprintf('输出为\n')
-disp(yt)
-end
+subplot(2,1,1)
+plot(t,x)
+title('零输入时的状态变量');
+xlabel('Time-sec'); ylabel('Response-value');
+subplot(2,1,2)
+plot(t,y)
+title('零输入时的系统输出')
+
+
 
