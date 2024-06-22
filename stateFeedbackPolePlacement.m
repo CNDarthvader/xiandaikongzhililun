af1d413b5692d651de84bd3617d5895e1a8d6625
Caoyang Shao <2963684236@qq.com>
2024-06-22 11:34:54 +0800

    stateFeedbackPolePlacementNO1
    增加了绘图功能

--- stateFeedbackPolePlacement.m
+++ stateFeedbackPolePlacement.m
@@ -1,14 +1,34 @@
-function stateFeedbackPolePlacement(A, B)
-    % 计算系统的阶数，即矩阵A的行数
-    systemOrder = size(A, 1);
-    fprintf('系统的阶数为 %d，因此您需要输入 %d 个极点。\n', systemOrder, systemOrder);
-    desiredPoles = input('请输入期望的闭环极点，格式如[-1, -2]: ');
-    % 使用place函数计算状态反馈矩阵K
-    K = place(A, B, desiredPoles);
-    A_cl = A - B*K;
-    % 显示结果
-    disp('状态反馈矩阵K:');
-    disp(K);
-    disp('闭环状态方程矩阵A_cl:');
-    disp(A_cl);
+function stateFeedbackPolePlacement(A,B,C,D)
+a = A;
+b = B;
+c = C;
+d = D;
+disp('The rank of controllability matrix')
+rc = rank(ctrb(a,b))
+systemOrder = size(A, 1);
+fprintf('系统的阶数为 %d，因此您需要输入期望极点的个数为 %d 。', systemOrder, systemOrder);
+ran2 = randi([1,10],systemOrder,1);
+fprintf('例如%s \n',mat2str(ran2))
+p = input('输入初始状态：');
+k=place(a,b,p)
+a1=a-b*k;b1=b*k(1);c1=c;d1=d;
+figure(1)
+step(a1,b1,c1,d1)
+title('Step Responese of Designed Servo System')
+figure(2)
+[y,x,t]=step(a1,b1,c1,d1);
+plot(t,x)
+title('Step Responese Curves for x1,x2,x3,x4')
+grid on
+disp('The rank of controllability matrix')
+rc = 4;
+num=[4.55 0];
+den=[1 0.182 -31.18 -4.45];
+G1=tf(num,den);
+G2=tf([8.5 99.4 300],[1 0]);
+G=series(G2,G1);
+H1=1;
+G0=feedback(G,H1)
+GG=zpk(G0)
+g=ss(a1,b1,c1,d1)
 end
